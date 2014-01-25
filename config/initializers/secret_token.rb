64193129b19b9108574aea757300671df6286576
Rishi Dhanaraj <rishid97@gmail.com>
2014-01-25 01:12:57 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swiftpost::Application.config.secret_key_base = '551fdf114a37ab764c5578312b75a58d4abe74889fa67e34565bc7da3d3cfdedda01085ca24e8e79e944261b4ca3285f489843c564dc3dd33c793f6350019325'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Swiftpost::Application.config.session_store :cookie_store, key: '_swiftpost_session'
