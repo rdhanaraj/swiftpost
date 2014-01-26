9c506529d6c2aed1d78b18d99b7a481e9a1613a3
Jaydeep <jdsingh94@gmail.com>
2014-01-26 03:04:19 -0800

    figaro the lob api, finish lob template controller

--- app/controllers/lob_controller.rb
+++ app/controllers/lob_controller.rb
@@ -1,5 +1,5 @@
 class LobController < ApplicationController
-  @lob = Lob(api_key: "live_45276d9bcbb1e49e4512926ec100bfe2e6d" )
+  @lob = Lob(api_key: ENV['LOB_API_KEY'] )
 
   def create_address_from_order(order)
     @lob.addresses.create(
@@ -11,37 +11,36 @@ class LobController < ApplicationController
       zip:            order.zip)
   end
 
-  def create_object_from_data
+  def create_object_from_data(lob_object)
     @lob.objects.create(
-      name:,
-      file:,
-      setting_id:)
+      name:       lob_object.name,
+      file:       lob_object.file_location,
+      setting_id: lob_object.setting_id)
   end
 
-  def create_custom_postcard_from_data
+  def create_custom_postcard_from_data(data)
     @lob.postcards.create(
-      name:,
-      to:,
-      from:,
-      front:,
-      back:)
+      name:     data.job_name,
+      to:       data.receiver,
+      from:     current_user,
+      front:    data.front_file,
+      back:     data.back_file)
   end
 
-  def @lob.postcards.create(
-    name:,
-    to:,
-    from:,
-    front:,
-    message:)
+  def create_message_postcard_from_data(data)
+    @lob.postcards.create(
+      name:     data.job_name,
+      to:       data.receiver,
+      from:     data.sender,
+      message:  data.message,
+      back:     data.back_file)
   end
 
-  def create_job_from_form_data
+  def create_job_from_form_data(data)
     @lob.jobs.create(
-      name:,
-      from:,
-      to:,
-      objects:)
+      name:     data.job_name,
+      from:     data.sender,
+      to:       data.receiver,
+      objects:  data.objects)
   end
-
-
 end
