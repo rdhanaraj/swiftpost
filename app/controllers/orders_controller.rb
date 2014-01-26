4869ae6370159129ed61afd870ad5e8bbec6dd10
Rishi Dhanaraj <rishid97@gmail.com>
2014-01-26 09:39:28 -0800

    Deprecate spreadsheet functionality, fix payment solution

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,11 +2,12 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy] 
   before_action :correct_user, only: [:edit, :update, :destroy]
 
-  def import
-    Order.import params[:spreadsheet]
-  end
+  # def import
+  #   Order.import params[:spreadsheet]
+  # end
 
   def show
+    @order
   end
 
   def index
@@ -25,17 +26,14 @@ class OrdersController < ApplicationController
   def create
     @order = current_user.orders.build order_params
 
-
-    if params[:step2] == 'spreadsheet-uploader'
-      upload_spreadsheet
-    else
+    if params[:step1] == 'demographic-selector'
       select_demographic
     end
 
     if @order.save
-      redirect_to @order, notice: 'Order was successfully created!'
+      redirect_to @order, notice: 'Please make payment to complete order.'
     else
-      render action: 'new'
+      redirect_to new_order_path, notice: 'Order not successful, try again.'
     end
   end
 
@@ -47,11 +45,11 @@ class OrdersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @order = Order.find(params[:id])
+      @order = Order.find_by_id(params[:id])
     end
 
     def correct_user
-      @order = current_user.orders.find_by(id: params[:id])
+      @order = current_user.orders.find_by_id(params[:id])
       redirect_to orders_path, notice: "Not your order!" if @order.nil?
     end
 
@@ -67,12 +65,12 @@ class OrdersController < ApplicationController
       end
     end
 
-    def upload_spreadsheet
-      import if params[:spreadsheet]
-    end
+    # def upload_spreadsheet
+    #   import if params[:spreadsheet]
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:design, :user_id, :city, :state, :zipcode, :sender_city, :sender_state, :sender_zipcode, :starting_age, :ending_age) if params[:order]
+      params.require(:order).permit(:design, :user_id, :city, :state, :zipcode, :sender_city, :sender_state, :sender_zipcode, :starting_age, :ending_age, :cap, :setting, :order_date, :cardholder_name, :credit_card_number, :cvc, :expiration) if params[:order]
     end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -8,39 +8,41 @@ class Order < ActiveRecord::Base
   attr_accessor :credit_card_number, :cvc, :expiration, :cardholder_name
   has_attached_file :design, styles: {thumbnail: "60x60#"}
 
-  validates_presence_of :sender_city, :sender_state, :sender_zipcode
   validates_attachment :design, presence: true,
     :content_type => { :content_type => "application/pdf" }
 
   def expenses
     case setting
       when 'Black and White Document'
-        return cap * 0.99
+        return 15 * 0.99
       when 'Color Document'
-        return cap * 1.49
+        return 15 * 1.49
       when 'Glossy Color Flyer'
-        return cap * 2.53
+        return 15 * 2.53
+      else
+        return 15 * 0.99
     end
   end
   
-  def self.import(file)
-    spreadsheet = open_spreadsheet(file)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      recipient = Recipient.new
-      recipient.update_attributes(name: spreadsheet.row(i)[1], address: spreadsheet.row(i)[2], city: spreadsheet.row(i)[3], state: spreadsheet.row(i)[4], country: 'US')
-      recipient.order_id = params[:id]
-      recipient.save!
-    end
-  end
+  # def self.import(file)
+  #   spreadsheet = open_spreadsheet(file)
+  #   header = spreadsheet.row(1)
+  #   (2..spreadsheet.last_row).each do |i|
+  #     recipient = Recipient.new
+  #     recipient.update_attributes(name: spreadsheet.row(i)[1], address: spreadsheet.row(i)[2], city: spreadsheet.row(i)[3], state: spreadsheet.row(i)[4])
+  #     recipient.order_id = params[:id]
+  #     recipient.save!
+  #   end
+  #   @order.cap = spreadsheet.last_row - 1
+  # end
 
-  def self.open_spreadsheet(file)
-    case File.extname(file.original_filename)
-      when ".csv" then Csv.new(file.path, nil, :ignore)
-      when ".xls" then Excel.new(file.path, nil, :ignore)
-      when ".xlsx" then Excelx.new(file.path, nil, :ignore)
-    end
-  end
+  # def self.open_spreadsheet(file)
+  #   case File.extname(file.original_filename)
+  #     when ".csv" then Csv.new(file.path, nil, :ignore)
+  #     when ".xls" then Excel.new(file.path, nil, :ignore)
+  #     when ".xlsx" then Excelx.new(file.path, nil, :ignore)
+  #   end
+  # end
 
   def create_address(city, state, zip)
     call_address = Addressable::URI.new(
